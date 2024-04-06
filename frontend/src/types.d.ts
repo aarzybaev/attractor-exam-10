7a3db807376aa79dc67987433c3629bca71af341
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-06 19:15:31 +0600

    created types.d

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,18 +1,41 @@
-export interface Message {
-  author: string;
-  message: string;
-  image: string | null;
-  createdAt?: string;
-}
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axiosApi from './axiosApi.ts';
 
-export interface MessageWithID extends Message {
+export interface NewsApi {
   id: string;
+  newsHeader: string;
+  image: string | null;
+  createdAt: string;
 }
 
+export interface NewsItem extends NewsApi {
+  newsContent: string;
+}
 
-
-export interface MessageMutation {
-  author: string;
-  message: string;
+export interface NewsFormMutation {
+  newsHeader: string;
+  newsContent: string;
   image: File | null;
 }
+
+
+export interface CommentsApi {
+  id: string;
+  newsID: string;
+  author: string;
+  commentText: string;
+}
+
+export interface CommentsFormMutation {
+  newsID: string;
+  author: string;
+  commentText: string;
+}
+
+// export const fetchComments = createAsyncThunk<CommentsApi[]>(
+//   'comments/fetchAll',
+//   async () => {
+//     const {data: comments} = await axiosApi.get<CommentsApi[]>('/news');
+//     return comments;
+//   }
+// );
